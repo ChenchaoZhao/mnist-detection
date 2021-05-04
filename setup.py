58870718b8e12f25a221b468540e93e514aca3f2
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-05-04 17:01:57 -0400

    version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="mnist-detection",
-    version="0.1.4",
+    version="0.1.5",
     author="Chenchao Zhao",
     author_email="chenchao.zhao@gmail.com",
     description="Simulated MNIST detection dataset",
